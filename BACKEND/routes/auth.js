29b2b534c5303044cc12943b0042cb46f31f8766
Muhammad Huzaifa Shah <shahhu823@gmail.com>
2026-07-30 22:50:55 +0500

    Some collecton are added

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -1,30 +1,38 @@
 const express = require("express");
-const User=require("../models/Users");
-const { body, validationResult } = require('express-validator');
-const router =express.Router();
+const User = require("../models/Users");
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
 
-
-router.post('/',body('name','Enter a valid Name.').isLength({ min: 5 }),body('email','Enter avlid Email').isEmail(),body('password','Password must be atleast 5 characters').isLength({ min: 5 }),(req,res)=>{
-   const result =validationResult(req);
+router.post(
+  "/createUser",
+  body("name", "Enter a valid Name.").isLength({ min: 5 }),
+  body("email", "Enter avlid Email").isEmail(),
+  body("password", "Password must be atleast 5 characters").isLength({
+    min: 5,
+  }),
+  async (req, res) => {
+    const result = validationResult(req);
     if (!result.isEmpty()) {
-        return res.status(400).json({ errors: result.array() });
+      return res.status(400).json({ errors: result.array() });
     }
-    User.create({
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exist." });
+      }
+      user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-    }).then(user => res.json(user)).catch(err=>{console.log(err);
-        if (err.code === 11000) {
-        return res.status(400).json({
-            error: "Email already exists"
-        });
+      });
+      return res.status(201).json({ user });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
     }
+  },
+);
 
-    return res.status(500).json({
-        error: "Internal Server Error"
-    });
-    })
-
-},);
-
-module.exports=router;
+module.exports = router;
